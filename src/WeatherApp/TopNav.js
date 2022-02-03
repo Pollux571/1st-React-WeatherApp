cb1712ce5f01649c4c8a97f706280dc01151c22f
Pollux571 <88760754+Pollux571@users.noreply.github.com>
2022-02-03 21:51:31 +0300

    build

---
+++ src/WeatherApp/TopNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TopNav = () => {
+  return (
+  <>
+             <div className='topNav'>
+                 <h3>Suleyman IT | Weather App</h3>
+             </div>
+  </>
+
+  );
+};
+
+export default TopNav;
